833f8476fb38b1e69566a5d0fb2c3f68226457f6
Michael Sainz <michaelsainz@github.com>
2020-01-06 07:55:38 -0800

    update

--- .github/actions/CombineFunctionFiles/entrypoint.sh
+++ .github/actions/CombineFunctionFiles/entrypoint.sh
@@ -4,6 +4,6 @@ echo "Current value of input: $1"
 echo "Current ENV:"
 printenv
 
-for i in ./src/*.ps1;do cat $i >> ./src/MyModule.psm1;done
+echo "for i in ${INPUT_SCRIPTPATH}*.ps1;do cat $i >> ${INPUT_SCRIPTPATH}MyModule.psm1;done"
 echo "Contents of PSM1:"
 cat ./src/MyModule.psm1
